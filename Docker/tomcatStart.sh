4c0ab868211a5012b32ffccdea9bdad8a1db222f
Martin <it.atsbanksinarmas.com@martin-mac>
2017-10-04 16:08:46 +0700

    Tomcat seems to works

--- Docker/tomcatStart.sh
+++ Docker/tomcatStart.sh
@@ -1,4 +1,1 @@
-curdir=${PWD}
-# mkdir tomcat
-# ln -s ../WebContent ./tomcat/DocumentManager
-docker run --rm -it -p 127.0.0.1:8080:8080 -v $curdir/tomcat:/usr/local/tomcat/webapps tomcat:8.0
+docker run --rm -it -p 127.0.0.1:8080:8080 -v ${PWD}/tomcat/DocumentManager:/usr/local/tomcat/webapps/DocumentManager tomcat:8.0
