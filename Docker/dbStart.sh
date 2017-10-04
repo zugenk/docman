f96e46eb131ce13cfb51be6cb91d0dddbc6247d8
Martin <it.atsbanksinarmas.com@martin-mac>
2017-10-04 19:28:18 +0700

    All works using Docker

--- Docker/dbStart.sh
+++ Docker/dbStart.sh
@@ -1,2 +1,1 @@
-curdir=${PWD}
-docker run --rm -it  --name postgres-docman -p 127.0.0.1:5432:5432  -v $curdir/postgres-Data:/var/lib/postgresql/data -e POSTGRES_ROOT_PASSWORD=admin -e POSTGRES_USER=docman -e POSTGRES_PASSWORD=docman -e POSTGRES_DB=docmanDB -e POSTGRES_INITDB_ARGS="--encoding=UTF8" -d postgres:9.5
+docker run --net docman-nw  --rm -it  --name postgres-docman -p 127.0.0.1:5432:5432  -v ${PWD}/postgres-Data:/var/lib/postgresql/data -e POSTGRES_ROOT_PASSWORD=admin -e POSTGRES_USER=docman -e POSTGRES_PASSWORD=docman -e POSTGRES_DB=docmanDB -e POSTGRES_INITDB_ARGS="--encoding=UTF8" -d postgres:9.5
