e21ec33b22a451897b2c4cbe96ba69553363c267
Martin <it.atsbanksinarmas.com@martin-mac>
2017-10-09 18:54:28 +0700

    Login Rest

--- src/com/app/module/rest/LoginController.java
+++ src/com/app/module/rest/LoginController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.bson.Document;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,49 +16,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.app.module.basic.LoginManager;
+import com.app.module.basic.PassportManager;
 import com.app.shared.ApplicationFactory;
 
 
 
 
 @Controller
-@RequestMapping("/docmgr/v1")
+@RequestMapping("/v1")
 public class LoginController {
 	private org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass().getName());
 	
-	@RequestMapping(value = "/login")
-	public String login(
-			@RequestHeader(value="Accept") String accept,
-			@RequestHeader(value="Accept-Language") String acceptLanguage,
-			@RequestHeader(value="Authorisation") String bauth,
-			@RequestHeader(value="ipassport") String ipass,
+	@RequestMapping(value = "action/login")
+	public String login(@RequestHeader HttpHeaders httpHeaders,
+//			@RequestHeader(value="Accept") String accept,
+//			@RequestHeader(value="Accept-Language") String acceptLanguage,
+//			@RequestHeader(value="Authorization") String basicAuth,
+//			@RequestHeader(value="ipassport") String ipassport,
+	
 			HttpServletResponse response) {
 		
-		if(ipass !=null) {
+		String ipassport =httpHeaders.getFirst("ipassport");
+		log.debug("Try Login REST Api... [");
+		Document iPassDoc=null;
+		if(ipassport !=null) {
 			log.debug("Login with ipassport");
-			LoginManager.
+			try {
+				iPassDoc=PassportManager.checkPassport(ipassport);
+			} catch (Exception e) {
+			}
 		} 
+		if (iPassDoc==null && httpHeaders.getFirst("Authorization")!=null) {
+			try {
+				log.debug("Login with basic auth =["+httpHeaders.getFirst("Authorization")+"]");
+				LoginManager.loginWithBasicAuth(httpHeaders.getFirst("Authorization"));
+							
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
 		
 		return null;
 	}
 	
-	@RequestMapping(value="action/login",method = {RequestMethod.POST})
-	public @ResponseBody ResponseEntity doLogin(@RequestBody final Map userMap) {
-		log.debug("doLogin!");
-		
-		Map map = new HashMap();
-		// klo ipassport gak ketemu return true, trus lanjut ke bawah (login proses)
-
-		ResponseEntity<Map> responseMapFilter = ControllerMapFilter.getInstance().preFilterV3(userMap, "/login");
-		if(responseMapFilter.getStatusCode() != HttpStatus.OK) return responseMapFilter;
-		
-		if(ApplicationFactory.isEmpty(challenge) 
-				&& ApplicationFactory.isEmpty(sessionCode)){
-			map.put("responseCode", "01");
-			map.put("responseMessage", "failed");
-			return new ResponseEntity<Map>(map, HttpStatus.BAD_REQUEST);
-		}
 	
-		
-	}
 }
