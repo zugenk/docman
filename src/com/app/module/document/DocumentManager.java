3cc22178f24474ce04028deb45fb08999147a912
Martin <it.atsbanksinarmas.com@martin-mac>
2017-10-19 01:22:26 +0700

    fix gitIgnore

--- src/com/app/module/document/DocumentManager.java
+++ src/com/app/module/document/DocumentManager.java
@@ -1,5 +1,23 @@
 package com.app.module.document;
 
-public class DocumentManager {
+import com.app.docmgr.service.DocumentService;
+import com.app.docmgr.service.ForumService;
+import com.app.shared.PartialList;
 
+public class DocumentManager {
+	public static int itemPerPage=20;
+
+	public static PartialList getForumList(int start){
+		PartialList resultList=null;
+		try {
+			String filterParam=null; 
+			String orderParam=" ORDER BY document.id ASC ";
+			resultList= DocumentService.getInstance().getPartialList(filterParam, orderParam, 0, itemPerPage);
+			//if(!resultList.isEmpty()) return true;
+		} catch (Exception e) {
+//			e.printStackTrace();
+		}
+		return resultList;
+	}
+	
 }
